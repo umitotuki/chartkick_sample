4cc5a92327166aba0d8b9206d31e64a8114279f8
umitotuki <umitukiuk@gmail.com>
2024-08-06 12:12:35 +0000

    修正

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   resources :rugby_world_cup_host_countries
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   
-  resources :rugby_world_cup_host_countries, only: [:index,:show,:edit,:create,:destroy,:update]
+  root :to =>"rugby_world_cup_host_countries#index"
 end
